808fd72887829731393a8e5fe665e8af9f5cab1c
Duncan Tormey <duncantormey@gmail.com>
2022-11-09 21:31:15 -0800

    Added files

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import reverse_geocode
+
+response = requests.get("https://shakeshack.com/locations#/")
+soup = BeautifulSoup(response.text, 'html.parser')
+geo_locations = soup.find_all("div", {"class": "geolocation-location"})
+df = pd.DataFrame([e.attrs for e in geo_locations])
+df['data-lat'] = df['data-lat'].astype(float)
+df['data-lng'] = df['data-lng'].astype(float)
+df["location"] = df.apply(lambda r: reverse_geocode.search(((r["data-lat"], r["data-lng"]),))[0], axis=1)
+df["country_code"] = df.location.apply(lambda x: x["country_code"])
+df["city"] = df.location.apply(lambda x: x["city"])
+df["country"] = df.location.apply(lambda x: x["country"])
+df = df[['data-lat', 'data-lng', 'country', 'country_code', 'city']]
+
+
